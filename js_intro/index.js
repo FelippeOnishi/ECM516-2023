715b17a7cf4e6a7560725a48fa3ea2c34fc26613
FelippeOnishi <felippe.onishi@gmail.com>
2023-03-11 09:59:11 -0300

    Closure JS

--- js_intro/index.js
+++ js_intro/index.js
@@ -171,49 +171,223 @@
 // }
 // console.log(ehPar(10))
 
-// Closures
-/*Uma função pode ser atribuída a uma variavel*/
-let umaFuncao = function(){
-    console.log("Fui armazenada em uma variável")
-}
-umaFuncao()
-/* f recebe uma função como parâmetro e, por isso é uma função de alta ordem,
-    Por devolver uma função, g também é de alta ordem */
-    function f(funcao){
-        // Chamada de função
-        // Note com a tipagem dinâmica tem seu preço
-        funcao()
-    }
+// //Closures
 
-function g(){
-    function outraFuncao(){
-        console.log("Fui criada por g")
-    }
-    return outraFuncao
-}
+// /*uma função pode ser atribuída a uma variável*/
+// // let umaFuncao = function () {
+// //     console.log("Fui armazenada em uma variável")
+// // }
+// // umaFuncao()
 
-// f pode ser chamada assim
-f(function(){
-    console.log("Estou sendo passada para f")
-})
+// /* f recebe uma função como parâmetro e, por isso, é
+//    uma função de alta ordem.
+//    Por devolver uma função, g também é de alta ordem */
 
-// e g pode ser chamada assim
-const gResult = g()
-gResult()
+//    function f (funcao){
+//     //chamada da função
+//     //note com a tipagem dinâmica tem seu preço
+//     funcao()
+// }
 
-//e assim também
-g()()
+// function g() {
+//     function outraFuncao(){
+//         console.log("Fui criada por g")
+//     }
+//     return outraFuncao;
+// }
 
-//outros testes
-/* f chama g, que somente devolve uma funcao;
-    nada é exibido*/
-f(g)
+// //f pode ser chamada assim
+// //f(function() {
+// //    console.log("Estou sendo passada para f")
+// //})
 
-/**f chama a função devolvida por g
- * "Fui criada por g" é exibido **/
-f(g())
+// //e g pode ser chamada assim
+// //const gResult = g()
+// //gResult()
 
-/**f tenta chamar o que a função criada por g
- * devolve. Ela não devolve nada. Por isso,
- * um erro acontece - somente em tempo de execução
- */
+// //e assim também
+// //g()()
+
+// //outros testes
+// /** f chama g, que somente devolve uma função; 
+//  * nada é exibido */
+// //f(g)
+
+// /**f chama a função devolvida por g
+//  * "Fui criada por g" é exibido
+//  */
+// //f(g())
+
+// /**
+//  * f tenta chamar o que a função criada por g
+//  * devolve. Ela não devolve nada. Por isso,
+//  * um erro acontece - somente em tempo de execução
+//  */
+// //f(g()())
+
+// //o que acontece?
+// f(1)
+
+// function f() {
+//     let nome = 'João';
+//     function g() {
+//         console.log(nome);
+//     }
+//     g()
+// }
+// f()
+
+// function ola() {
+//     let nome = 'João';
+//     return function () {
+//         console.log('Olá, João');
+//     }
+// }
+
+// let olaResult = ola();
+// /*perceba que aqui a função ola já terminou.
+//  É de se esperar que a variável nome já não
+// possa ser acessada.*/
+// olaResult();
+
+// //também vale com parâmetros
+// function saudacoesFactory(saudacao, nome) {
+//     return function () {
+//         console.log(saudacao + ', ' + nome);
+//     }
+// }
+// let olaJoao = saudacoesFactory('Olá', 'João');
+// let tchauJoao = saudacoesFactory('Tchau', 'João');
+// olaJoao();
+// tchauJoao();
+
+
+// function eAgora() {
+//     let cont = 1;
+//     function f1() {
+//         console.log(cont);
+//     }
+//     cont++;
+//     function f2() {
+//         console.log(cont);
+//     }
+//     //JSON contendo as duas funções
+//     return { f1, f2 }
+// }
+
+// let eAgoraResult = eAgora();
+
+// /* neste momento, a funcao eAgora já
+// executou por completo e a variável
+// cont já foi incrementada. Seu valor final
+// é mantido e, assim, ambas f1 e f2 exibirão 2.
+// */
+// eAgoraResult.f1();
+// eAgoraResult.f2();
+
+
+// let pessoa = {
+//     nome: "João",
+//     idade: 17,
+// }
+// //o acesso a propriedades pode ser feito com ponto
+// console.log("Me chamo " + pessoa.nome);
+// //e com [] também
+// console.log("Tenho " + pessoa["idade"] + " anos");
+
+// let pessoaComEndereco = {
+//     nome: "Maria",
+//     idade: 21,
+//     endereco: {
+//         logradouro: "Rua B",
+//         numero: 121,
+//     },
+// };
+// console.log(
+//     `Sou ${pessoaComEndereco.nome},
+//      tenho ${pessoaComEndereco.idade} anos
+//      e moro na rua ${pessoaComEndereco.endereco.logradouro} 
+//      número ${pessoaComEndereco["endereco"]["numero"]}`
+// );
+
+// let concessionaria = {
+//     cnpj: "00011122210001-45",
+//     endereco: {
+//         logradouro: "Rua A",
+//         numero: 10,
+//         bairro: "Vila J",
+//     },
+//     veiculos: [
+//         {
+//             marca: "Ford",
+//             modelo: "Ecosport",
+//             anoDeFabricacao: 2018,
+//         },
+//         {
+//             marca: "Chevrolet",
+//             modelo: "Onix",
+//             anoDeFabricacao: 2020,
+//         },
+//         {
+//             marca: "Volkswagen",
+//             modelo: "Nivus",
+//             anoDeFabricacao: 2020,
+//         },
+//     ],
+// };
+// for (let veiculo of concessionaria.veiculos) {
+//     console.log(`Marca: ${veiculo.marca}`);
+//     console.log(`Modelo: ${veiculo.modelo}`);
+//     console.log(`Ano de Fabricação:
+//       ${veiculo.anoDeFabricacao}`);
+// }
+
+// let calculadora = {
+//     //pode ser arrow function
+//     soma: (a, b) => a + b,
+//     //e função comum também
+//     subtracao: function (a, b) {
+//         return a - b;
+//     },
+// };
+// console.log(`2 + 3 = ${calculadora.soma(2, 3)}`);
+// console.log(`2 - 3 = ${calculadora.subtracao(2, 3)}`);
+
+
+// console.log('Eu primeiro')
+// console.log("Agora eu")
+// console.log("Sempre vou ser a última...:(")
+
+// const a = 2 + 7
+// const b = 5
+// //só faz sentido se os valores a e b já estiverem disponíveis
+// console.log(a + b)
+
+// function demorada() {
+//     const atualMais2Segundos = new Date().getTime() + 2000
+//     //não esqueça do ;, única instrução no corpo do while
+//     while (new Date().getTime() <= atualMais2Segundos);
+//     const d = 8 + 4
+//     return d
+// }
+// const a = 2 + 3
+// const b = 5 + 9
+// //função será executada depois de, pelo menos, 500 milissegundos
+// setTimeout(function () {
+//     const d = demorada()
+//     console.log(d)
+// }, 500)
+
+// //enquanto isso, essas linhas prosseguem executando
+// //sem ficar esperando
+// const e = 2 + a + b
+// console.log(e)
+
+// setTimeout(function () {
+//     console.log('dentro da timeout')
+// }, 0)
+// const a = new Date().getTime() + 1000
+// //não esqueça do ;, única instrução no corpo do while
+// while (new Date().getTime() <= a);
+// console.log('fora da timeout')
+
